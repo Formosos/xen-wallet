7a7c31527b675bc6d388763eaa1c3b39c7f73585
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2022-10-17 20:20:36 +0300

    move some of the functionality to a factory

--- test/Wallet.ts
+++ test/Wallet.ts
@@ -2,26 +2,72 @@ import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { XENCrypto, XENWallet, XENWalletFactory } from "../typechain-types";
 
 describe("Wallet", function () {
+
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
   async function deployWalletFixture() {
     // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
+    const [_owner, _otherAccount] = await ethers.getSigners();
+
+    const XEN = await ethers.getContractFactory("XENCrypto");
+    const _xen = await XEN.deploy();
+
     const Wallet = await ethers.getContractFactory("XENWallet");
-    const wallet = await Wallet.deploy();
-    return { wallet, owner, otherAccount };
+    const _wallet = await Wallet.deploy();
+    await _wallet.initialize(_xen.address);
+
+    const Factory = await ethers.getContractFactory("XENWalletFactory");
+    const _factory = await Factory.deploy(_xen.address, _wallet.address);
+
+    return { _xen, _wallet, _factory, _owner, _otherAccount };
   }
 
+  let xen : XENCrypto, wallet : XENWallet, factory : XENWalletFactory, owner : SignerWithAddress;
+
+  beforeEach(async function () {
+    const { _xen, _wallet, _factory, _owner } = await loadFixture(deployWalletFixture);
+
+    xen = _xen;
+    wallet = _wallet;
+    factory  = _factory;
+    owner = _owner;
+  });
+
   describe("Deployment", function () {
-    it("Should set the right owner", async function () {
-      const { wallet, owner } = await loadFixture(deployWalletFixture);
-      // expect(await wallet.owner()).to.equal(owner.address);
-      await wallet.createWallet(0);
+    it("Should set the right values", async function () {
+
+      const walletXen = await wallet.XENCrypto();
+      const factoryXen = await factory.XENCrypto();
+      const factoryDeployer = await factory.deployer();
+      const factoryImplementation = await factory.implementation();
+
+      expect(walletXen).to.equal(xen.address);
+      expect(factoryXen).to.equal(xen.address);
+      expect(factoryDeployer).to.equal(owner.address);
+      expect(factoryImplementation).to.equal(wallet.address);
+    });
+  });
+
+  describe("Cloning", function () {
+    beforeEach(async function () {
+      
     });
 
-    // TODO: Write tests for contract methods
+    it("Sets right mapping", async function () {
+      const id = 1;
+      const salt = await factory.getSalt(id);
+      const addressToBe = await factory.getDeterministicAddress(salt);
+
+      await factory.createWallet(id);
+
+      const storedAddress = await factory.reverseAddressResolver(addressToBe);
+
+      expect(storedAddress).to.equal(owner.address);
+    });
   });
 });
