6b6ba830d255a25800dced1be2bb9b8bda9e8261
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2022-10-21 20:34:53 +0300

    Add deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,10 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
+import dotenv from "dotenv";
+import "@nomiclabs/hardhat-etherscan";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -11,6 +15,20 @@ const config: HardhatUserConfig = {
         runs: 1,
       },
     },
+  },
+  networks: {
+    hardhat: {
+      gasPrice: 0,
+      initialBaseFeePerGas: 0,
+      chainId: 31337,
+    },
+    goerli: {
+      url: process.env.GOERLI_PROVIDER_URL,
+      accounts: [process.env.GOERLI_PRIVATE_KEY!],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
   gasReporter: {
     enabled: false,
