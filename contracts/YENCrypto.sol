528e539e5f68ff466b07a1736e8a3caeb46a57ae
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2022-10-23 11:54:44 +0300

    Add separete rescue address

--- contracts/YENCrypto.sol
+++ contracts/YENCrypto.sol
@@ -4,16 +4,10 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract YENCrypto is ERC20 {
-    uint256 internal constant LAUNCH_TIME = 1666254094;
-
     address public minter;
 
-    constructor(address _minter) ERC20("", "") {
+    constructor(address _minter) ERC20("YEN", "YEN") {
         minter = _minter;
-    }
-
-    function currentDay() external view returns (uint256) {
-        return (block.timestamp - LAUNCH_TIME) / 1 days;
     }
 
     function mint(address account, uint256 amount) external {
