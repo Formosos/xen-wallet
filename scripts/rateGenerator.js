09f6935864acc44568ec818192ef212578461344
Philipp Braun <3589810@gmail.com>
2022-11-06 01:41:52 +0100

    feat(rewardMultiplier): Update multiplier and generator scripts

--- scripts/rateGenerator.js
+++ scripts/rateGenerator.js
@@ -1,8 +1,9 @@
-let original = 10000;
-const precisionBooster = 100000;
+const precisionMultiplier = 10 ** 9;
 
-const calcRate = (_original, _elapsedWeeks) => {
-  let _current = (2 * _original) / (1 + _elapsedWeeks);
+const calcRate = (_precisionMultiplier, _elapsedWeeks) => {
+  // integrate a * 0.95^x from 0 to infinity = 2
+  // solve => a = 0.102586724
+  let _current = _precisionMultiplier * 0.102586724;
   let _cumulative = _current;
   for (let i = 0; i < _elapsedWeeks; ++i) {
     _current = (_current * 95) / 100;
@@ -13,10 +14,9 @@ const calcRate = (_original, _elapsedWeeks) => {
 
 const rates = [];
 
-for (let i = 0; i < 1000; i++) {
-  const res = calcRate(original, i);
-  const percentage = Math.floor(res / original * precisionBooster);
-  rates.push(percentage);
-
-  console.log(`precalculatedRates[${i}] = ${percentage};`)
+for (let i = 0; i < 500; i++) {
+  const rewardMultiplier = Math.floor(calcRate(precisionMultiplier, i));
+  rates.push(rewardMultiplier);
+  console.log(`weeklyRewardMultiplier[${i}] = ${rewardMultiplier};`)
 }
+
