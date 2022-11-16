f922fd1125aacfd593a07bfcac92a46e77c3e09a
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2022-11-16 21:09:55 +0200

    remove 'upgrades' package

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,5 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "@openzeppelin/hardhat-upgrades";
 import dotenv from "dotenv";
 import "@nomiclabs/hardhat-etherscan";
 
